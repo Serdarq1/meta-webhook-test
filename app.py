fa0c80647f831fdac36c635b00ddec890a80e1e3
Serdar Ziya Akova <serdar.akova5@gmail.com>
2026-03-31 10:22:02 +0300

    Initalize

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from fastapi import FastApi, Request, HTTPException
+from fastapi.responses import PlainTextResponse, JSONResponse
+from dotenv import load_dotenv
+
+load_dotenv()
+app = FastApi()
+
+VERIFY_TOKEN=os.getenv("META_VERIFY_TOKEN")
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+@app.get("/webhooks/meta")
+async def verify_webhook(request: Request):
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return PlainTextResponse(content=challenge or "", status_code=200)
+    
+    raise HTTPException(status_code=403, detail="Verification Failed.")
+
+@app.post("/webhooks/meta")
+async def receive_webhook(request: Request):
+    data = await request.json()
+    print("Incoming webhook: ", data)
+    return JSONResponse({"received": True})
